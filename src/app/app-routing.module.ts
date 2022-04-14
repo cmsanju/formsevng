e34f26e99f78d7ce68e3bf3a595747ff0f17d125
cmsanju <lict.sanju@gmail.com>
2022-04-14 20:09:32 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home-page', component: HomeComponent},
+  {path:'login-page', component:LoginpageComponent},
+  {path:'sign-up', component:SignuppageComponent},
+  {path:'', redirectTo:'/home-page', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import{Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formsevng';
+  title = 'HOME PAGE';
+
+  msg = "WELCOME TO ANGUALR IMPLEMENTATION";
+
+  constructor(private router:Router)
+  {
+
+  }
+
+  newPage():void{
+    this.router.navigate(['/home-page']);
+  }
 }
