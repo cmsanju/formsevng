a29606bac8d1a07ec7cc13ffec8ad4f2c2b497dd
cmsanju <lict.sanju@gmail.com>
2022-04-18 18:45:57 +0530

    adaa

--- src/app/signuppage/signuppage.component.ts
+++ src/app/signuppage/signuppage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-signuppage',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignuppageComponent implements OnInit {
 
+  public sdata:Student[]=[
+    {id:1, name:'User1'},
+    {id:2, name:'User2'},
+    {id:3, name:'User3'},
+    {id:4, name:'User4'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
